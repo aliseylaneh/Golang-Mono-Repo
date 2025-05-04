dddb827b8898403616075b17c5d44281afcf8fed
Ali Seylaneh <aliseylaneh@gmail.com>
2025-05-04 13:17:57 +0330

    type assertion checking

--- go/practices/pointers/pointers.go
+++ go/practices/pointers/pointers.go
@@ -8,11 +8,13 @@ type Product struct {
 }
 
 func typeChecker(arg interface{}) {
-	switch arg {
-	case arg.(Product):
+	switch value := arg.(type) {
+	case Product:
 		fmt.Println("Type is product")
-	case arg.(*Product):
+	case *Product:
 		fmt.Println("Type is a pointer product")
+	default:
+		fmt.Println("Unknow type:", value)
 	}
 }
 func failedUpdate(g *int) {
@@ -30,11 +32,12 @@ func makePointer[T any](t T) *T {
 	return &t
 }
 func main() {
-	//product := &Product{name: makePointer("name"), address: "test"}
-	//fmt.Println(*product)
-	var g int = 10
-	failedUpdate(&g)
-	fmt.Println(g)
-	update(&g)
-	fmt.Println(g)
+	product := &Product{name: makePointer("name"), address: "test"}
+	fmt.Println(*product)
+	typeChecker(product)
+	//var g int = 10
+	//failedUpdate(&g)
+	//fmt.Println(g)
+	//update(&g)
+	//fmt.Println(g)
 }
