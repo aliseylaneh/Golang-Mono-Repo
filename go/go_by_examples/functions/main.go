9445d997d83576c5f0fdf428eb3e6f7e940f7b59
Ali Seylaneh <aliseylaneh@gmail.com>
2025-05-10 13:11:56 +0330

    practice multiple return value function

--- go/go_by_examples/functions/main.go
+++ go/go_by_examples/functions/main.go
@@ -11,6 +11,29 @@ func myFunction(word string, function functionAsArg) {
 	returnedValue := function(word)
 	fmt.Println(returnedValue)
 }
+
+// Variadic function
+// Multiple return type for function
+// This function returns the hit target of a coin for the relevant day earned sale.
+// Also, it will bring back if it was eligible to be a progress sale for that coin.
+func eligibleTarget(targetSales []int, dayEarnedSale int) (int, bool) {
+	if targetSales[0] > dayEarnedSale {
+		return 0, false
+	}
+	var winner int
+	for i, targetSale := range targetSales {
+		if i == len(targetSales)-1 {
+			winner = targetSale
+		}
+		if targetSale <= dayEarnedSale && dayEarnedSale < targetSales[i+1] {
+			winner = targetSale
+			return winner, true
+		}
+	}
+	return winner, true
+}
 func main() {
-	myFunction("Ali Seylaneh", wordPrinter)
+	targetSales := []int{1200, 1400, 1550, 1600}
+	fmt.Println(eligibleTarget(targetSales, 1500))
+	//myFunction("Ali Seylaneh", wordPrinter)
 }
