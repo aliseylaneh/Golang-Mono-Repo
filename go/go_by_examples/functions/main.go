de351814a57b80ab6314516809720eb21ae6615e
Ali Seylaneh <aliseylaneh@gmail.com>
2025-05-10 13:24:11 +0330

    practice variadic functions along side type switch

--- go/go_by_examples/functions/main.go
+++ go/go_by_examples/functions/main.go
@@ -13,6 +13,21 @@ func myFunction(word string, function functionAsArg) {
 }
 
 // Variadic function
+func variadicFunction(names ...any) {
+	for i, name := range names {
+		switch valueType := name.(type) {
+		case string:
+			fmt.Printf("The index %v is a %T and the value is %v\n", i, valueType, name)
+		case int:
+			fmt.Printf("The index %v is an %T and the value is %v\n", i, valueType, name)
+		case float32:
+			fmt.Printf("The index %v is a %T and the value is %v\n", i, valueType, name)
+		default:
+			fmt.Printf("The type of index %v is a %T and the value is %v, and it's not listed in our system\n", i, valueType, name)
+		}
+	}
+}
+
 // Multiple return type for function
 // This function returns the hit target of a coin for the relevant day earned sale.
 // Also, it will bring back if it was eligible to be a progress sale for that coin.
@@ -33,7 +48,12 @@ func eligibleTarget(targetSales []int, dayEarnedSale int) (int, bool) {
 	return winner, true
 }
 func main() {
+	// Multiple return value function
 	targetSales := []int{1200, 1400, 1550, 1600}
 	fmt.Println(eligibleTarget(targetSales, 1500))
-	//myFunction("Ali Seylaneh", wordPrinter)
+	// Function as args
+	myFunction("Ali Seylaneh", wordPrinter)
+
+	// Variadic functions
+	variadicFunction(3, 4, "Ali Seylaneh", float32(3.5), 4.6)
 }
