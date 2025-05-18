bbf363af7afefa73a9efc9dc920d3b953b325eae
Ali Seylaneh <aliseylaneh@gmail.com>
2025-05-18 09:36:30 +0330

    practicing interface implementation

---
+++ go/go_by_examples/interfaces/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r *rect) perim() float64 {
+	return (r.height + r.width) * 2
+}
+func (r *rect) area() float64 {
+	return r.width * r.height
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c *circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+func (c *circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func printPerim(shape geometry) {
+	fmt.Printf("shape is %T, perm is: %v\n", shape, shape.perim())
+}
+func printArea(shape geometry) {
+	fmt.Printf("shape is %T, area is: %v\n", shape, shape.area())
+
+}
+
+func main() {
+	rectangle := rect{width: 30, height: 10}
+	printArea(&rectangle)
+	printPerim(&rectangle)
+	circular := circle{radius: 30}
+	printArea(&circular)
+	printPerim(&circular)
+}
