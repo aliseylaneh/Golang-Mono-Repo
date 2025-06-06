7440c6c5e58c6c40b830e6cdfd54c8fc8f9303fb
Ali Seylaneh <aliseylaneh@gmail.com>
2025-06-06 16:11:38 +0330

    practice buffered channels

--- go/go_by_examples/channels/main.go
+++ go/go_by_examples/channels/main.go
@@ -1,13 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+//func main() {
+//	messages := make(chan string, 2)
+//	go func() {
+//		messages <- "Ping 2"
+//		messages <- "Ping 3"
+//	}()
+//	for _ = range 2 {
+//		fmt.Println(<-messages)
+//	}
+//}
 
 func main() {
-	messages := make(chan string)
-	go func() {
-		messages <- "Ping 2"
-	}()
-	fmt.Println(<-messages)
+	messages := make(chan string, 2)
+
+	messages <- "Ping 2"
+	messages <- "Ping 3"
+
+	for _ = range 2 {
+		fmt.Println(<-messages)
+	}
 }
